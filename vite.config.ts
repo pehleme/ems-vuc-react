f5927289958b32822faa587b239c1577ff2b558e
Pedro Leme <32654571+pehleme@users.noreply.github.com>
2022-04-14 01:30:16 -0300

    feat update lint ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
-import react from '@vitejs/plugin-react';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import * as path from 'path';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import * as path from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: path.resolve(__dirname, '.env'),
+  envDir: path.resolve(__dirname, ".env"),
   build: {
     rollupOptions: {
-      input: path.resolve(__dirname, 'src/main.prod.tsx'),
+      input: path.resolve(__dirname, "src/main.prod.tsx"),
     },
   },
-  plugins: [
-    tsconfigPaths(),
-    reactRefresh(),
-    react()
-  ]
-})
+  plugins: [tsconfigPaths(), reactRefresh(), react()],
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,17 @@
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    test: {
-        globals: true,
-        setupFiles: "__tests__/setupTests.ts",
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: path.resolve(__dirname, "config/setupTests.ts"),
+    clearMocks: true,
+    coverage: {
+      reporter: ["cobertura", "html", "lcov", "text-summary", "text"],
     },
-    plugins: [
-        tsconfigPaths(),
-        react()
-    ],
-})
+  },
+  plugins: [tsconfigPaths(), react()],
+});

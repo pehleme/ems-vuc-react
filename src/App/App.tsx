287160c8a185920b273eecb9a1d5507b3c74bb16
Pedro Leme <32654571+pehleme@users.noreply.github.com>
2022-04-17 21:50:20 -0300

    remove twind

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,7 +5,6 @@ import { fetchFindTodos } from "~/components";
 import { GlobalProvider } from "~/contexts";
 import { TodoModel } from "~/data/models";
 import { theme } from "~/styles/mui/theme";
-import { tw } from "twind";
 
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 
@@ -29,11 +28,7 @@ function App() {
         <GlobalProvider>
           <Box className="container flex flex-col p-5 mx-auto space-y-5 bg-slate-100">
             {todos.map(({ title, description, id, color }) => (
-              <Box
-                key={id}
-                sx={{ borderColor: color, color }}
-                className={tw`bg-white shadow-lg p-5 border-l-4 rounded-lg flex flex-row font-bold text-center uppercase`}
-              >
+              <Box key={id} sx={{ borderColor: color, color }}>
                 <div>{title}</div>
                 <div>{description}</div>
               </Box>

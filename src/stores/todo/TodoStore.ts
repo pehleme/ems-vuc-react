11c4a27c776699b3c735708a30834357290b2f31
Pedro Leme <32654571+pehleme@users.noreply.github.com>
2022-04-25 10:32:44 -0300

    Update file name

--- src/stores/todo/TodoStore.ts
+++ src/stores/todo/TodoStore.ts
@@ -1,9 +1,8 @@
 import { createStore } from "effector";
 
-// import { findTodos } from "./TodoEvents";
+import { findTodos } from "./TodoEvents";
 import { TodoState } from "./TodoState";
 
-export const $todoStore = createStore<TodoState>({});
-// .on(findTodos, (state) => ({
-//   ...state,
-// }));
+export const $todoStore = createStore<TodoState>({}).on(findTodos, (state) => ({
+  ...state,
+}));
